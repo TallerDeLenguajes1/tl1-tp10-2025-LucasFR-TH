19d317dbd55ff4391278ce1b5b25218a4ff62126
LucasFR-TH <lucas.f.roldan@gmail.com>
2025-06-17 15:36:52 -0300

    separo d manera correcta los archivos

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioTarea;
+
+// Utilizando la clase HttpClient, realiza una petición GET asíncrona al siguiente
+// endpoint: https://jsonplaceholder.typicode.com/todos/
+
+HttpClient client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+
+response.EnsureSuccessStatusCode();
+
+// A la respuesta JSON de la API, deserialízala en una lista de objetos de tu clase
+// Tarea (List<Tarea>)
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+// Recorra la lista de tareas y muestra en la consola el título y el estado
+// (completada/pendiente) de cada una de forma clara y legible, Al mostrar los
+// resultados en consola, filtra la lista para mostrar primero todas las tareas
+// pendientes y luego las completadas.
+
+foreach (var Tar in listTareas)
+{
+    if (Tar.completed) {
+        Console.WriteLine("Tareas Completadas:")
+        Console.WriteLine("ID Usuario: " + Tar.userId + " ID Tarea: " + Prov.id + " Tarea: " + Prov.title);    
+    }
+}
+
+foreach (var Tar in listTareas)
+{
+    if (!Tar.completed) {
+        Console.WriteLine("Tareas No Completadas:")
+        Console.WriteLine("ID Usuario: " + Tar.userId + " ID Tarea: " + Prov.id + " Tarea: " + Prov.title);    
+    }
+}
+
+
+// A la lista completa de tareas y serialízala nuevamente a formato JSON y guarda el
+// resultado en un archivo llamado tareas.json en el directorio de ejecución de la
+// aplicación.
+string jsonString = JsonSerializer.Serialize(Tar);
+
--- Tareas/Tareas.cs
+++ Tareas/Tareas.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Text.Json;
+namespace EspacioTarea;
 
 // Defina una clase en C# llamada Tarea que represente la estructura de los objetos
 // devueltos por la API.
@@ -10,40 +9,4 @@ public class Tarea { // IMPLICITAMENTE HAY UN COSNTRUTOR VACIO
     public string title { get; set;} // 1 cokmpletada y 0 no
     public bool completed { get; set; }
 }
-
-// Utilizando la clase HttpClient, realiza una petición GET asíncrona al siguiente
-// endpoint: https://jsonplaceholder.typicode.com/todos/
-
-HttpClient client = new HttpClient();
-
-HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
-
-response.EnsureSuccessStatusCode();
-
-// A la respuesta JSON de la API, deserialízala en una lista de objetos de tu clase
-// Tarea (List<Tarea>)
-string responseBody = await response.Content.ReadAsStringAsync();
-List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
-
-// Recorra la lista de tareas y muestra en la consola el título y el estado
-// (completada/pendiente) de cada una de forma clara y legible, Al mostrar los
-// resultados en consola, filtra la lista para mostrar primero todas las tareas
-// pendientes y luego las completadas.
-
-foreach (var Tar in listTareas)
-{
-    if (Tar.completed) {
-        Console.WriteLine("Tareas Completadas:")
-        Console.WriteLine("ID Usuario: " + Tar.userId + " ID Tarea: " + Prov.id + " Tarea: " + Prov.title);    
-    }
-}
-
-foreach (var Tar in listTareas)
-{
-    if (!Tar.completed) {
-        Console.WriteLine("Tareas No Completadas:")
-        Console.WriteLine("ID Usuario: " + Tar.userId + " ID Tarea: " + Prov.id + " Tarea: " + Prov.title);    
-    }
-}
-
 
